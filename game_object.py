a5e6f250bda330e0f8dfdbb7c7c0e9b63c227932
Q <q@q-continuum.net>
2015-07-08 00:19:09 -0600

    Changed up the game object to have its own constructor, and set up a sound if one is passed in. Maybe this is the wrong layer of abstraction for this.

--- game_object.py
+++ game_object.py
@@ -1,9 +1,18 @@
 import attr
 
-@attr.s
+@attr.s(init=False)
 class GameObject(object):
 	name = attr.ib(default="")
-	size = attr.ib(default=attr.Factory(lambda: [1, 1]))
+	size = attr.ib(default=attr.Factory(lambda: (1, 1)))
 	world = attr.ib(default=None)
-	sound = attr.ib(default=None)
 	fixed = attr.ib(default=False)
+
+	def __init__(self, world=None, name="", size=(1, 1), fixed=True, sound=None, *args, **kwargs):
+		super(GameObject, self).__init__(*args, **kwargs)
+		self.name = name
+		self.world = world
+		self.size = size
+		self.fixed = fixed
+		if sound is not None:
+			sound = game.sound_manager.play(sound, looping=True)
+		self.sound = sound
