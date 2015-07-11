ee978995e02932eaac457d794595f7b9c7fb0ee7
Q <q@q-continuum.net>
2015-07-11 03:39:34 -0600

    more to try and get exits to work, they don't, and aren't close.

--- room.py
+++ room.py
@@ -11,12 +11,13 @@ class Room(object):
 			(size[1] + 2, size[0] + 2),
 			(size[1] + 2, 1)
 		]
-		exit_coords = [tuple(i.values()[0]) for i in exits]
+		exit_coords = [tuple([j+1 for j in i.values()[0]]) for i in exits]
 		filled_tiles = []
 		filled_tiles.extend(tiles_for_wall(corners[0], corners[1], exit_coords))
 		filled_tiles.extend(tiles_for_wall(corners[1], corners[2], exit_coords))
 		filled_tiles.extend(tiles_for_wall(corners[2], corners[3], exit_coords))
 		filled_tiles.extend(tiles_for_wall(corners[3], corners[0], exit_coords))
+		self.exit_coords = exit_coords
 		self.filled_tiles = filled_tiles
 		self.floor = floor
 		if exits is None:
@@ -25,8 +26,8 @@ class Room(object):
 		self.exits = exits
 		bodies = []
 		for tile in filled_tiles:
-			position = (tile[0] - 0.5, tile[1] - 0.5)
-			shape = b2.polygonShape(box=(1, 1))
+			position = (tile[0], tile[1])
+			shape = b2.polygonShape(box=(0.5, 0.5))
 			bodies.append(self.world.world.CreateStaticBody(shapes=shape, position=position))
 		self.bodies = bodies
 		#dictionary of {coords: object}
