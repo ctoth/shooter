9c67d984dcb8b095e2ba727e5ff42e5b3b748fe1
Q <q@q-continuum.net>
2015-07-17 14:01:02 -0600

    Updated sound manager to work with new libaudioverse changes including adding support for play_async.

--- sound.py
+++ sound.py
@@ -9,7 +9,7 @@ class SoundManager(object):
 	def __init__(self, output_device=-1, sounds_path='sounds'):
 		self.sim = libaudioverse.Simulation()
 		self.sim.set_output_device(output_device)
-		self.world = libaudioverse.SimpleEnvironmentNode(self.sim, "default")
+		self.world = libaudioverse.EnvironmentNode(self.sim, "default")
 		self.world.default_panning_strategy.value=libaudioverse.PanningStrategies.hrtf
 		self.world.output_channels.value= 2
 		self.world.default_max_distance.value = 20
@@ -18,19 +18,15 @@ class SoundManager(object):
 		self.sounds_path = sounds_path
 
 	def play(self, filename, source, looping=False, position=(0, 0, 0)):
-		filename = os.path.join(self.sounds_path, filename)
-		if os.path.isdir(filename):
-			filename = os.path.join(filename, random.choice(self.list_sounds_in_directory(filename)))
 		sound = libaudioverse.BufferNode(self.sim)
-		sound_buffer = self.sounds.get(filename)
-		if not sound_buffer:
-			sound_buffer = libaudioverse.Buffer(self.sim)
-			sound_buffer.load_from_file(filename)
-		sound.set_buffer(sound_buffer)
+		sound_buffer = self.get_buffer(filename)
+		sound.buffer = sound_buffer
 		sound.connect(0, source, 0)
+		if not looping:
+			sound.end_event = _disconnecter
 		sound.looping.value = looping
 		self.sounds[filename] = sound_buffer
-		return sound
+		return Sound(sound)
 
 	def list_sounds_in_directory(self, directory):
 		res = []
@@ -43,3 +39,30 @@ class SoundManager(object):
 
 	def create_source(self):
 		return libaudioverse.SourceNode(self.sim, self.world)
+
+	def get_buffer(self, filename):
+		filename = os.path.join(self.sounds_path, filename)
+		if os.path.isdir(filename):
+			filename = os.path.join(filename, random.choice(self.list_sounds_in_directory(filename)))
+		sound_buffer = self.sounds.get(filename)
+		if not sound_buffer:
+			sound_buffer = libaudioverse.Buffer(self.sim)
+			sound_buffer.load_from_file(filename)
+		return sound_buffer
+
+	def play_async(self, filename, x=0, y=0, z=0):
+		position = (x, z, y)
+		buffer = self.get_buffer(filename)
+		self.world.play_async(buffer, *position)
+
+
+def _disconnecter(node):
+	node.disconnect(0)
+
+class Sound(object):
+
+	def __init__(self, buffer_node):
+		self.buffer_node = buffer_node
+
+	def stop(self):
+		self.buffer_node.state = libaudioverse.NodeStates.stop
