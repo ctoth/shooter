2dce53c50453451600a0e97095aa5d93a577b3eb
Q <q@q-continuum.net>
2015-07-18 00:21:05 -0600

    Made exits atually end up where they should be

--- room.py
+++ room.py
@@ -15,7 +15,7 @@ class Room(object):
 		exit_coords = []
 		for exit in exits:
 			exit_pos = exit.values()[0]
-			exit_coords.append((exit_pos[0]+1, exit_pos[1]+1))
+			exit_coords.append((exit_pos[0]+1, exit_pos[1]))
 		filled_tiles = []
 		filled_tiles.extend(tiles_for_wall(corners[0], corners[1], exit_coords))
 		filled_tiles.extend(tiles_for_wall(corners[1], corners[2], exit_coords))
@@ -30,8 +30,8 @@ class Room(object):
 		self.exits = exits
 		bodies = []
 		for tile in filled_tiles:
-			position = (tile[0] - 0.5, tile[1] - 0.5)
-			shape = b2.polygonShape(box=(1.0, 1.0))
+			position = (tile[0] + 0.5, tile[1] + 0.5)
+			shape = b2.polygonShape(box=(0.5, 0.5))
 			bodies.append(self.world.world.CreateStaticBody(shapes=shape, position=position, userData=self))
 		self.bodies = bodies
 		#dictionary of {coords: object}
