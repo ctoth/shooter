c446329d56581965e4f1eae391170b0a9607e0b7
Q <q@q-continuum.net>
2015-07-21 13:50:41 -0600

    Give the default weapon a mass, we really need weapon templates

--- player.py
+++ player.py
@@ -19,7 +19,7 @@ class Player(entity.Entity):
 	def __init__(self, size=(0.5, 0.5), mass=100, *args, **kwargs):
 		super(Player, self).__init__(size=size, mass=mass, *args, **kwargs)
 		self.moving = False
-		gun = weapon.ProjectileWeapon(world=self.world, name="Gun", ammo_type="bullet", use_sound='rifle', size=(1, 0.1), position=self.position, cooldown=0.5)
+		gun = weapon.ProjectileWeapon(world=self.world, name="Gun", ammo_type="bullet", use_sound='rifle', size=(1, 0.1), position=self.position, cooldown=0.5, mass=30)
 		self.equip(gun)
 		self.turning = None
 		self.running = False
