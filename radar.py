0e49f2e8277140161f90a332136dda5f32de500a
Q <q@q-continuum.net>
2015-07-22 17:55:12 -0600

    Support for reading exits with the radar

--- radar.py
+++ radar.py
@@ -14,10 +14,17 @@ class Radar(object):
 			return
 		query = game.world.query(room[0], room[2])
 		items = [i.body.userData for i in query.fixtures if i.body.userData is not game.world and math_utils.distance(i.body.position, self.looker.position) > 0]
-		return items
+		def sort_by_distance(k):
+			return math_utils.distance(self.looker.position, k.position)
+		return sorted(items, key=sort_by_distance)
+
+	def get_surrounding_exits(self):
+		def sort_by_distance(k):
+			return math_utils.distance(self.looker.position, k)
+		return sorted(game.map.nearby_exits(self.looker.position), key=sort_by_distance)
 
 	def current_item(self):
-		items = self.get_surrounding_items()
+		items = self.get_surrounding_items() + self.get_surrounding_exits()
 		if not items:
 			return
 		self.index %= len(items)
@@ -35,11 +42,15 @@ class Radar(object):
 		item = self.current_item()
 		if item is None:
 			return
-		name = item.name
-		position = item.position
+		if isinstance(item, tuple):
+			name = "exit"
+			position = item
+		else:
+			name = item.name
+			position = item.position
 		position_vector = math_utils.vec_sub(position, self.looker.position)
 		direction = (math_utils.vec_to_angle(position_vector) - self.looker.facing) % 360
-		distance = math_utils.distance(self.looker.position, item.position)
+		distance = math_utils.distance(self.looker.position, position)
 		game.output.output("%s: %.2f meters at %d degrees" % (name, distance, direction))
 		game.sound_manager.play_async('beep.wav', *position)
 
