6767383b9035fa5d7fb2cebda68b4b20484f86ff
camlorn <camlorn38@gmail.com>
2015-09-20 12:44:59 -0400

    Let Libaudioverse handle culling.

--- npc.py
+++ npc.py
@@ -25,14 +25,6 @@ class NPC(entity.Entity):
 		self.is_being_attacked = False
 
 	def tick(self):
-		if self.position.distance(game.player.position) > self.activation_distance:
-			self.sound_source.state.value = libaudioverse.NodeStates.paused
-			if self.weapon is not None:
-				self.weapon.sound_source.state.value = libaudioverse.NodeStates.paused
-			return
-		self.sound_source.state.value = libaudioverse.NodeStates.playing
-		if self.weapon is not None:
-			self.weapon.sound_source.state.value = libaudioverse.NodeStates.playing
 		if self.ambient_sound is not None and not self.ambient_sound.is_playing():
 			self.ambient_sound.play()
 		super(NPC, self).tick()
